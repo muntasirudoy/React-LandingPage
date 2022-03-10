d895cd9b0ad0335a47245f3cb2d7ff66f5bd7388
muntasirudoy <muntasirudoy4@gmail.com>
2022-03-11 00:38:13 +0600

    asd

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -150,8 +150,8 @@ export const Contact = (props) => {
       <div id='footer'>
         <div className='container text-center'>
           <p>
-            &copy; 2020 Issaaf Kattan React Land Page Template. Design by{' '}
-            <a href='http://www.templatewire.com' rel='nofollow'>
+            &copy; 2022 React Land Page Template. Design by muntasir udoy
+            <a href='http://www.muntasirudoy.com' rel='nofollow'>
               TemplateWire
             </a>
           </p>
